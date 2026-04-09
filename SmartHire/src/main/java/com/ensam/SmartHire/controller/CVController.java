ea80f0222862086654aed0935ed73268b66317d5
oumaimabelabbas <oumaimabelabbas2003@gmail.com>
2026-04-09 22:11:10 +0100

    upload_cv_linked_to_backend

--- SmartHire/src/main/java/com/ensam/SmartHire/controller/CVController.java
+++ SmartHire/src/main/java/com/ensam/SmartHire/controller/CVController.java
@@ -25,6 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 @RequestMapping("/cv")
 public class CVController {
 
--- SmartHire/src/main/java/com/ensam/SmartHire/security/SecurityConfig.java
+++ SmartHire/src/main/java/com/ensam/SmartHire/security/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -22,10 +27,26 @@ public class SecurityConfig{
     private MyUserDetailsService userDetailsService;
     @Autowired
     private JwtFilter jwtFilter;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
 
         http
+                .cors(cors->{})
                 .csrf(csrf -> csrf.disable()) //stateless no need for csrf
                 .headers(header->header.frameOptions(frame->frame.disable()))
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
